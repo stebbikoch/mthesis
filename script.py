d5e0b7676a6c12102ea38d88840356a50a8af3ef
StefanKoch <stebbitokoch@gmail.com>
2021-11-30 15:00:26 +0100

    modified script.py

--- script.py
+++ script.py
@@ -8,6 +8,11 @@ from scipy.sparse.linalg import eigsh
 from scipy.sparse.linalg import eigs
 from Reproduce import main
 import time
+# build d_function thing
+x = integer_inequality(np.array([100, 100, 1]))
+x.all_numbers(49,d_given = [2,5,8,10,12,15,20,25, 30, 35, 40, 49])
+x.save_to_json('2d_100_100_1')
+# do the rest
 start = time.time()
 exponent = np.arange(16)
 q_values = 10 ** (-exponent / 3)
